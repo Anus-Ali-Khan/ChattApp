47c4677f7b2d3ba156b463266d793333fd2515f4
Anus Ali Khan <anusalikhan1405@gmail.com>
2023-08-15 22:49:36 +0500

    working on Auth Page

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,16 +1,16 @@
 import React from "react";
-import { auth, provider } from "../firebase-config.js";
+import { auth, provider } from "../firebase-config";
 import { signInWithPopup } from "firebase/auth";
 
-const signInWithGoogle = async () => {
-  await signInWithPopup(auth, provider);
-};
-
 function Auth() {
+  const signInWithGoogle = async () => {
+    await signInWithPopup(auth, provider);
+  };
+
   return (
     <div className="auth">
-      <p> Sign In With Google To Continue</p>
-      <button onClick={signInWithGoogle}>Sign In with Google</button>
+      <p>Sign In With Google To Continue</p>
+      <button onClick={signInWithGoogle}>Sign In With Google</button>
     </div>
   );
 }
