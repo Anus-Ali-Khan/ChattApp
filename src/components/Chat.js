e305c020a134cd26339aaaf4cfe76ff471eaff03
Anus Ali Khan <anusalikhan1405@gmail.com>
2023-08-17 23:26:05 +0500

    working on Chattpage

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -12,13 +12,19 @@ import "../styles/Chat.css";
 
 function Chat({ room }) {
   const [newMessage, setNewMessage] = useState("");
+  const [messages, setMessages] = useState([]);
   const messagesRef = collection(db, "messages");
 
   useEffect(() => {
-    const queryMessages = query(messagesRef, where("room", "==", rooms));
-    onSnapshot(queryMessages, (snapshot) => {
-      console.log("NEW MESSAGE ");
+    const queryMessages = query(messagesRef, where("room", "==", room));
+    const unsuscribe = onSnapshot(queryMessages, (snapshot) => {
+      let messages = [];
+      snapshot.forEach((doc) => {
+        messages.push({ ...doc.data(), id: doc.id });
+      });
+      setMessages(messages);
     });
+    return () => unsuscribe();
   }, []);
 
   const handleSubmit = async (e) => {
