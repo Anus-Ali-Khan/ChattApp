5bd9f6aab767e92c7ae4b66925c2f4507d3f1b7a
Anus Ali Khan <anusalikhan1405@gmail.com>
2023-08-19 00:09:50 +0500

    Signout functionality added and app completed

--- src/App.js
+++ src/App.js
@@ -5,11 +5,20 @@ import { useState, useRef } from "react";
 import Chat from "./components/Chat";
 import Cookies from "universal-cookie";
 const cookies = new Cookies();
+import { signOut } from "firebase/auth";
+import { auth } from "./firebase-config";
 
 function App() {
   const [isAuth, setIsAuth] = useState(cookies.get("auth-token"));
   const [room, setRoom] = useState(null);
   const roomInputRef = useRef(null);
+
+  const signUserOut = async () => {
+    await signOut(auth);
+    cookies.remove("auth-token");
+    setIsAuth(false);
+    setRoom(null);
+  };
 
   if (!isAuth) {
     return (
@@ -35,7 +44,7 @@ function App() {
         )}
       </div>
       <div className="sign-out">
-        <button>SignOut</button>
+        <button onClick={signUserOut}>SignOut</button>
       </div>
     </>
   );
